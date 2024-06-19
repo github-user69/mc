e11a215bb36d52fee2f0d21ba0cdd0286c5c102a
github-user69 <155996723+github-user69@users.noreply.github.com>
2024-06-19 20:12:34 +0530

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,331 @@
+/*
+const mineflayer = require('mineflayer');
+const Movements = require('mineflayer-pathfinder').Movements;
+const pathfinder = require('mineflayer-pathfinder').pathfinder;
+const { GoalBlock } = require('mineflayer-pathfinder').goals;
+
+const config = require('./settings.json');
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Bot Is Ready')
+});
+
+app.listen(3000, () => {
+  console.log('server started');
+});
+
+function createBot() {
+   const bot = mineflayer.createBot({
+      username: config['bot-account']['username'],
+      password: config['bot-account']['password'],
+      auth: config['bot-account']['type'],
+      host: config.server.ip,
+      port: config.server.port,
+      version: config.server.version,
+   });
+
+   bot.loadPlugin(pathfinder);
+   const mcData = require('minecraft-data')(bot.version);
+   const defaultMove = new Movements(bot, mcData);
+   bot.settings.colorsEnabled = false;
+
+   bot.once('spawn', () => {
+      console.log('\x1b[33m[BotLog] Bot joined to the server', '\x1b[0m');
+
+      if (config.utils['auto-auth'].enabled) {
+         console.log('[INFO] Started auto-auth module');
+
+         var password = config.utils['auto-auth'].password;
+         setTimeout(() => {
+            bot.chat(`/register ${password} ${password}`);
+            bot.chat(`/login ${password}`);
+         }, 500);
+
+         console.log(`[Auth] Authentification commands executed.`);
+      }
+
+      if (config.utils['chat-messages'].enabled) {
+         console.log('[INFO] Started chat-messages module');
+         var messages = config.utils['chat-messages']['messages'];
+
+         if (config.utils['chat-messages'].repeat) {
+            var delay = config.utils['chat-messages']['repeat-delay'];
+            let i = 0;
+
+            let msg_timer = setInterval(() => {
+               bot.chat(`${messages[i]}`);
+
+               if (i + 1 == messages.length) {
+                  i = 0;
+               } else i++;
+            }, delay * 1000);
+         } else {
+            messages.forEach((msg) => {
+               bot.chat(msg);
+            });
+         }
+      }
+
+      const pos = config.position;
+
+      if (config.position.enabled) {
+         console.log(
+            `\x1b[32m[BotLog] Starting moving to target location (${pos.x}, ${pos.y}, ${pos.z})\x1b[0m`
+         );
+         bot.pathfinder.setMovements(defaultMove);
+         bot.pathfinder.setGoal(new GoalBlock(pos.x, pos.y, pos.z));
+      }
+
+      if (config.utils['anti-afk'].enabled) {
+         bot.setControlState('jump', true);
+         if (config.utils['anti-afk'].sneak) {
+            bot.setControlState('sneak', true);
+         }
+      }
+   });
+
+   bot.on('chat', (username, message) => {
+      if (config.utils['chat-log']) {
+         console.log(`[ChatLog] <${username}> ${message}`);
+      }
+   });
+
+   bot.on('goal_reached', () => {
+      console.log(
+         `\x1b[32m[BotLog] Bot arrived to target location. ${bot.entity.position}\x1b[0m`
+      );
+   });
+
+   bot.on('death', () => {
+      console.log(
+         `\x1b[33m[BotLog] Bot has been died and was respawned ${bot.entity.position}`,
+         '\x1b[0m'
+      );
+   });
+
+   if (config.utils['auto-reconnect']) {
+      bot.on('end', () => {
+         setTimeout(() => {
+            createBot();
+         }, config.utils['auto-recconect-delay']);
+      });
+   }
+
+   bot.on('kicked', (reason) =>
+      console.log(
+         '\x1b[33m',
+         `[BotLog] Bot was kicked from the server. Reason: \n${reason}`,
+         '\x1b[0m'
+      )
+   );
+   bot.on('error', (err) =>
+      console.log(`\x1b[31m[ERROR] ${err.message}`, '\x1b[0m')
+   );
+}
+const mineflayer = require('mineflayer');
+const Movements = require('mineflayer-pathfinder').Movements;
+const pathfinder = require('mineflayer-pathfinder').pathfinder;
+const { GoalBlock } = require('mineflayer-pathfinder').goals;
+
+const config = require('./settings.json');
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Bot Is Ready')
+});
+
+app.listen(3000, () => {
+  console.log('server started');
+});
+
+function createBot() {
+   const bot = mineflayer.createBot({
+      username: config['bot-account']['username'],
+      password: config['bot-account']['password'],
+      auth: config['bot-account']['type'],
+      host: config.server.ip,
+      port: config.server.port,
+      version: config.server.version,
+   });
+
+   bot.loadPlugin(pathfinder);
+   const mcData = require('minecraft-data')(bot.version);
+   const defaultMove = new Movements(bot, mcData);
+   bot.settings.colorsEnabled = false;
+
+   bot.once('spawn', () => {
+      console.log('\x1b[33m[BotLog] Bot joined to the server', '\x1b[0m');
+
+      if (config.utils['auto-auth'].enabled) {
+         console.log('[INFO] Started auto-auth module');
+
+         var password = config.utils['auto-auth'].password;
+         setTimeout(() => {
+            bot.chat(`/register ${password} ${password}`);
+            bot.chat(`/login ${password}`);
+         }, 500);
+
+         console.log(`[Auth] Authentification commands executed.`);
+      }
+
+      if (config.utils['chat-messages'].enabled) {
+         console.log('[INFO] Started chat-messages module');
+         var messages = config.utils['chat-messages']['messages'];
+
+         if (config.utils['chat-messages'].repeat) {
+            var delay = config.utils['chat-messages']['repeat-delay'];
+            let i = 0;
+
+            let msg_timer = setInterval(() => {
+               bot.chat(`${messages[i]}`);
+
+               if (i + 1 == messages.length) {
+                  i = 0;
+               } else i++;
+            }, delay * 1000);
+         } else {
+            messages.forEach((msg) => {
+               bot.chat(msg);
+            });
+         }
+      }
+
+      const pos = config.position;
+
+      if (config.position.enabled) {
+         console.log(
+            `\x1b[32m[BotLog] Starting moving to target location (${pos.x}, ${pos.y}, ${pos.z})\x1b[0m`
+         );
+         bot.pathfinder.setMovements(defaultMove);
+         bot.pathfinder.setGoal(new GoalBlock(pos.x, pos.y, pos.z));
+      }
+
+      if (config.utils['anti-afk'].enabled) {
+         bot.setControlState('jump', true);
+         if (config.utils['anti-afk'].sneak) {
+            bot.setControlState('sneak', true);
+         }
+      }
+   });
+
+   bot.on('chat', (username, message) => {
+      if (config.utils['chat-log']) {
+         console.log(`[ChatLog] <${username}> ${message}`);
+      }
+   });
+
+   bot.on('goal_reached', () => {
+      console.log(
+         `\x1b[32m[BotLog] Bot arrived to target location. ${bot.entity.position}\x1b[0m`
+      );
+   });
+
+   bot.on('death', () => {
+      console.log(
+         `\x1b[33m[BotLog] Bot has been died and was respawned ${bot.entity.position}`,
+         '\x1b[0m'
+      );
+   });
+
+   if (config.utils['auto-reconnect']) {
+      bot.on('end', () => {
+         setTimeout(() => {
+            createBot();
+         }, config.utils['auto-recconect-delay']);
+      });
+   }
+
+   bot.on('kicked', (reason) =>
+      console.log(
+         '\x1b[33m',
+         `[BotLog] Bot was kicked from the server. Reason: \n${reason}`,
+         '\x1b[0m'
+      )
+   );
+   bot.on('error', (err) =>
+      console.log(`\x1b[31m[ERROR] ${err.message}`, '\x1b[0m')
+   );
+}
+
+const mineflayer = require('mineflayer');
+const Movements = require('mineflayer-pathfinder').Movements;
+const pathfinder = require('mineflayer-pathfinder').pathfinder;
+const { GoalBlock } = require('mineflayer-pathfinder').goals;
+
+const config = require('./settings.json');
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Bot Is Ready')
+});
+
+app.listen(3000, () => {
+  console.log('server started');
+});
+*/
+const mineflayer = require('mineflayer');
+const Movements = require('mineflayer-pathfinder').Movements;
+const pathfinder = require('mineflayer-pathfinder').pathfinder;
+const { GoalBlock } = require('mineflayer-pathfinder').goals;
+
+const config = require('./settings.json');
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Bot Is Ready')
+});
+
+app.listen(3000, () => {
+  console.log('server started');
+});
+
+function createBot() {
+  const bot = mineflayer.createBot({
+    username: config['bot-account']['username'],
+    password: config['bot-account']['password'],
+    auth: config['bot-account']['type'],
+    host: config.server.ip,
+    port: config.server.port,
+    version: config.server.version,
+  });
+
+  bot.loadPlugin(pathfinder);
+  const mcData = require('minecraft-data')(bot.version);
+  const defaultMove = new Movements(bot, mcData);
+  bot.settings.colorsEnabled = false;
+
+  bot.once('spawn', () => {
+    console.log('[BotLog] Bot joined the server');
+
+    // Circular movement code
+    const startPos = bot.entity.position;
+    const center = startPos.offset(0, 0, 0); // Adjust the center as needed
+    const radius = 5; // Adjust the radius as needed
+    let angle = 0;
+
+    setInterval(() => {
+      const targetX = center.x + radius * Math.cos(angle);
+      const targetZ = center.z + radius * Math.sin(angle);
+      const targetY = center.y; // Adjust the target Y position as needed
+
+      const goal = new GoalBlock(targetX, targetY, targetZ);
+      bot.pathfinder.setMovements(defaultMove);
+      bot.pathfinder.setGoal(goal);
+
+      angle += 0.1; // Adjust the angle increment as needed
+    }, 1000); // Adjust the timer interval as needed
+  });
+
+  // Set up event listeners and other functionality
+}
+
+createBot();
